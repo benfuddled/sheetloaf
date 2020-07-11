1a504ba312b3e96742c3072fc5f5ba3bf21130ea
Benjamin Richardson <21092217+benjrichardson@users.noreply.github.com>
2020-07-11 14:46:23 -0400

    Initial sass commit

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const sass = require('sass');
 
 // Skip the first two, I don't care.
 const args = process.argv.slice(2);
@@ -6,3 +7,13 @@ const args = process.argv.slice(2);
 console.log( "Hello!" );
 console.log(`Input given was ${args[0]}`);
 console.log(`All args are: ${args}`);
+
+var destination = 'test/output/thing.css';
+sass.render({
+    file: args[0],
+    sourceMap: true,
+    sourceMapEmbed: true,
+    outFile: destination
+}, function (err, result) {
+    console.log(result);
+});
