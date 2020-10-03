c0e0b65bd95529f3e637b100f3a487bc19af82eb
Ben Richardson <21092217+benjrichardson@users.noreply.github.com>
2020-10-03 11:18:11 -0400

    add support for stdin

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
 #!/usr/bin/env node
-//todo sourcemaps and semver and stdin
+ //todo sourcemaps and semver and stdin
 const sass = require('sass');
 const path = require('path');
 const fs = require('fs');
 const version = require('./package.json').version;
 const parser = require('./parser');
-const { Command } = require('commander');
+const {
+    Command
+} = require('commander');
 const postcss = require('postcss');
 const chokidar = require('chokidar');
 const program = new Command();
+
+let stdin = '';
 
 program.version(version, '-v, --version', 'Print the version of Sheetloaf.');
 
 // Angled brackets denote required argument, square denote optional
 program
-    .arguments('<sources...>') // can change source to []
+    .arguments('[sources...]')
     .description('🍖 Compile Sass to CSS and transform the output using PostCSS all in one command.')
     .action((source) => {
-        parser.expandGlob(source[0].split(','), function(entries) {
-            entries.forEach(function (filename) {
-                if (path.basename(filename).charAt(0) !== '_') {
-                    renderSheet(filename);
-                }
+        if (stdin) {
+            renderSheet(null, stdin);
+        } else {
+            parser.expandGlob(source[0].split(','), function (entries) {
+                entries.forEach(function (filename) {
+                    if (path.basename(filename).charAt(0) !== '_') {
+                        renderSheet(filename);
+                    }
+                });
             });
-        });
 
-        if (program.watch) {
-            chokidar.watch(source[0].split(','), {
-                usePolling: true,
-                interval: 500,
-                ignoreInitial: true,
-                awaitWriteFinish: {
-                    stabilityThreshold: 1500,
-                    pollInterval: 100
-                }
-            }).on('change', (changed) => {
-                console.log(`File changed: ${changed}`);
-        
-                parser.expandGlob(source[0].split(','), function(entries) {
-                    entries.forEach(function (filename) {
-                        if (path.basename(filename).charAt(0) !== '_') {
-                            renderSheet(filename);
-                        }
+            if (program.watch) {
+                chokidar.watch(source[0].split(','), {
+                    usePolling: true,
+                    interval: 500,
+                    ignoreInitial: true,
+                    awaitWriteFinish: {
+                        stabilityThreshold: 1500,
+                        pollInterval: 100
+                    }
+                }).on('change', (changed) => {
+                    console.log(`File changed: ${changed}`);
+
+                    parser.expandGlob(source[0].split(','), function (entries) {
+                        entries.forEach(function (filename) {
+                            if (path.basename(filename).charAt(0) !== '_') {
+                                renderSheet(filename);
+                            }
+                        });
+                    });
+                }).on('add', (added) => {
+                    console.log(`File added: ${added}`);
+
+                    parser.expandGlob(source[0].split(','), function (entries) {
+                        entries.forEach(function (filename) {
+                            if (path.basename(filename).charAt(0) !== '_') {
+                                renderSheet(filename);
+                            }
+                        });
                     });
                 });
-            }).on('add', (added) => {
-                console.log(`File added: ${added}`);
-        
-                parser.expandGlob(source[0].split(','), function(entries) {
-                    entries.forEach(function (filename) {
-                        if (path.basename(filename).charAt(0) !== '_') {
-                            renderSheet(filename);
-                        }
-                    });
-                });
-            });
+            }
         }
     });
 program
@@ -67,29 +75,49 @@ program
     .option('-w, --watch', 'Watch stylesheets and recompile when they change.')
     .option('--config <LOCATION>', 'Set a custom directory to look for a postcss config file.')
     .option('-u, --use <PLUGINS>', 'List of postcss plugins to use. Will cause sheetloaf to ignore any config files.');
-program.parse(process.argv);
-
 
 let postcssConfig = {
     plugins: []
 };
-if (program.use !== undefined) {
-    // If user specifies --use, ignore postcss config files.
-    program.use.split(',').forEach(function (plugin) {
-        postcssConfig.plugins.push(require(plugin));
-    });
+
+// https: //github.com/tj/commander.js/issues/137
+if (process.stdin.isTTY) {
+    program.parse(process.argv);
+    generatePostcssConfig();
 } else {
-    postcssConfig = parser.getPostCSSConfig(program.config);
+    process.stdin.on('readable', function () {
+        var chunk = this.read();
+        if (chunk !== null) {
+            stdin += chunk;
+        }
+    });
+    process.stdin.on('end', function () {
+        program.parse(process.argv);
+        generatePostcssConfig();
+    });
 }
 
-function renderSheet(filename) {
+function generatePostcssConfig() {
+    if (program.use !== undefined) {
+        // If user specifies --use, ignore postcss config files.
+        program.use.split(',').forEach(function (plugin) {
+            postcssConfig.plugins.push(require(plugin));
+        });
+    } else {
+        postcssConfig = parser.getPostCSSConfig(program.config);
+    }
+}
+
+function renderSheet(filename = null, stdin = null) {
+
+    if (stdin === null) {
+        console.log(`Rendering ${filename}...`);
+    }
 
     let destination;
-    if (program.dir) {
-        console.log(`Rendering ${filename}...`);
+    if (program.dir && stdin === null) {
         destination = parser.parseDest(filename, program.dir, program.base, program.ext);
     } else if (program.output) {
-        console.log(`Rendering ${filename}...`);
         destination = parser.parseDest(filename, program.output);
     } else {
         destination = '';
@@ -104,13 +132,20 @@ function renderSheet(filename) {
     //     outputStyle: program.style
     // });
 
-    sass.render({
-        file: filename,
+    let sassOptions = {
         sourceMap: program.sourceMap,
         sourceMapEmbed: program.sourceMap,
         outFile: destination,
         outputStyle: program.style
-    }, function (err, result) {
+    }
+
+    if (stdin !== null) {
+        sassOptions.data = stdin
+    } else {
+        sassOptions.file = filename
+    }
+
+    sass.render(sassOptions, function (err, result) {
         if (err === null) {
             postcss(postcssConfig.plugins).process(result.css.toString(), {
                 from: result.stats.entry,
@@ -119,15 +154,17 @@ function renderSheet(filename) {
             }).then(postedResult => {
                 if (destination !== '') {
                     try {
-                        fs.mkdirSync(path.dirname(destination), { recursive: true });
+                        fs.mkdirSync(path.dirname(destination), {
+                            recursive: true
+                        });
                     } catch (err) {
                         if (err.code !== 'EEXIST' || err.code !== 'EISDIR') throw err
                     }
-    
+
                     fs.writeFile(destination, postedResult.css, (err) => {
                         // throws an error, you could also catch it here
                         if (err) throw err;
-    
+
                         // success case, the file was saved
                         console.log(`Successfully written to ${destination}`);
                     })
